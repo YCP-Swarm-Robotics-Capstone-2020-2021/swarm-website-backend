3d6f27208b7eb2825fc5fd321a2e9540feae6777
cbrandt6 <cbrandt6@ycp.edu>
2020-09-05 13:16:21 -0400

    Admin serializer tests complete.

--- swarmdjango/core/unit_tests/test_admin_serializer.py
+++ swarmdjango/core/unit_tests/test_admin_serializer.py
@@ -5,16 +5,22 @@ from core.serializers.Serializers import AdminSerializer
 
 class AdminSerializerTest(TestCase):
     def setUp(self):
-
+        # Create attributes to be passed to an admin object
         self.admin_attributes = {
             'receiveUpdates': False
         }
+        # Create an admin object
         self.admin = Admin.objects.create(**self.admin_attributes)
+        # Create admin serializer
         self.serializer = AdminSerializer(instance=self.admin)
+        # Serializer data to test against
+        self.data = self.serializer.data
 
+    # Test serializer has correct fields
     def test_contains_fields(self):
-        data = self.serializer.data
+        self.assertEqual(set(self.data.keys()), {'receiveUpdates'})
 
-        self.assertEqual(set(data.keys()), {'receiveUpdates'})
-
+    # Test that fields contain correct data
+    def test_update_field_content(self):
+        self.assertFalse(self.data['receiveUpdates'])
 
