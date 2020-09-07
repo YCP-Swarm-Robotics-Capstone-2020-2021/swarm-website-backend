6521958892f260cd07c6a62ae58ef65db790fddd
cbrandt6 <cbrandt6@ycp.edu>
2020-09-06 20:22:08 -0400

    Modified Log model to have foreign keys for robots and runs

--- swarmdjango/core/models/Log.py
+++ swarmdjango/core/models/Log.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
-#from core.models import Robot
-#from core.models import Run
+from core.models import Robot
+from core.models import Run
+
 
 class Log(models.Model):
     dateTime = models.DateTimeField()
-    #robot = models.OneToOneField('Robot', on_delete=models.CASCADE)
-    #run = models.OneToOneField('Run', on_delete=models.CASCADE)
+    robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
     log = JSONField()
