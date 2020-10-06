7ba36c03cc9d7f1eb9c36171adc025e56900e2be
Rithonn <rithonn21@gmail.com>
2020-10-06 19:41:54 -0400

    Got it cheif

--- swarmdjango/core/views/userViewSet.py
+++ swarmdjango/core/views/userViewSet.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
@@ -18,12 +19,19 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False)
     def verify_password(self, request):
-        obj = request.data
-        obj2 = self.request.data
+
         obj3 = self.request.query_params.get('username')
+        obj4 = self.request.query_params.get('password')
         queryset = User.objects.all()
         user = queryset.filter(username=obj3)
         serialzer = serializers.UserSerializer(user[0])
 
-        return Response(serialzer.data, status=status.HTTP_200_OK)
+        hash = serialzer.data.get('password')
+
+        if check_password(obj4, hash):
+            return Response("Success", status=status.HTTP_200_OK)
+        else:
+            return Response("Fail", status=status.HTTP_400_BAD_REQUEST)
+
+
 
