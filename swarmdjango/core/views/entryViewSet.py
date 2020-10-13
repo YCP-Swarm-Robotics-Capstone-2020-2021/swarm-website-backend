5570ca57d743448ae41f354116d9771ecbaf53e8
Walrussuit101 <tjefferson@ycp.edu>
2020-10-13 17:00:12 -0400

    new method for entry viewset
    needed to make a custom delete method, the given one for a viewset does not delete the fields as well as the parent object

--- swarmdjango/core/views/entryViewSet.py
+++ swarmdjango/core/views/entryViewSet.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Entry
 from core.serializers import serializers
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 
 class EntryViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,17 @@ class EntryViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.EntrySerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = '__all__'
+
+    @action(detail=False)
+    def delete_entry(self, request, *args, **kwargs):
+        id = self.request.query_params.get('id')
+        entry_to_delete = Entry.objects.get(id=id)
+
+        # delete associated models, then the entry
+        entry_to_delete.log.all().delete()
+        entry_to_delete.comments.all().delete()
+        entry_to_delete.headings.all().delete()
+        entry_to_delete.sideBar.delete()
+
+        entry_to_delete.delete()
+        return Response(status=status.HTTP_200_OK)
