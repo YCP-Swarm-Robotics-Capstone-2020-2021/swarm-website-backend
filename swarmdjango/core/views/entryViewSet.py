21e72234177c66e39f3c8e0cb4eee5f195c0d1b0
Walrussuit101 <tjefferson@ycp.edu>
2020-10-14 21:42:20 -0400

    404 if the entry doesn't exist

--- swarmdjango/core/views/entryViewSet.py
+++ swarmdjango/core/views/entryViewSet.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from core.models import Entry
@@ -16,7 +17,7 @@ class EntryViewSet(viewsets.ModelViewSet):
     @action(detail=False)
     def delete_entry(self, request, *args, **kwargs):
         id = self.request.query_params.get('id')
-        entry_to_delete = Entry.objects.get(id=id)
+        entry_to_delete = get_object_or_404(Entry, id=id)
 
         # delete associated models, then the entry
         entry_to_delete.log.all().delete()
