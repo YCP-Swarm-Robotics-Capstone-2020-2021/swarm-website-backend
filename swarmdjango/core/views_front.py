169fda8e74bb3159d435422bb228372fc9ea2ce9
cbrandt6 <cbrandt6@ycp.edu>
2021-02-01 22:11:58 -0500

    Created a view for to be used for routing the front end

--- swarmdjango/core/views_front.py
+++ swarmdjango/core/views_front.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
 
-def wiki(request):
-    return render(request, "wiki")
+# def wiki(request):
+#     return render(request, 'wiki')
