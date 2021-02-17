2e790498c60ab62183881132aedaa2261b6b267a
cbrandt6 <cbrandt6@ycp.edu>
2021-02-17 17:47:35 -0500

    The method accepts the zip file, and is able to read it

--- swarmdjango/core/views/logViewSet.py
+++ swarmdjango/core/views/logViewSet.py
@@ -1,8 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
-
 from core.models import Log
 from core.serializers import serializers
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser, MultiPartParser
+from django.views.decorators.gzip import gzip_page
+from rest_framework import status
+from zipfile import ZipFile
 
 
 class LogViewSet(viewsets.ModelViewSet):
@@ -10,3 +15,17 @@ class LogViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.LogSerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ['dateTime', 'robot', 'run']
+    # parser_classes = [MultiPartParser]
+
+    # Method to accept the log file upload from the GCS
+    @gzip_page
+    @action(methods=['post'], detail=False)
+    def upload_log_zip(self, request):
+        # file_obj = request.data['file']
+        with open('upload.zip', 'wb+') as destination:
+            for chunk in request.FILES['file'].chunks():
+                destination.write(chunk)
+
+        with ZipFile('upload.zip') as upload:
+            return Response({"Message": "Uploaded."})
+        
