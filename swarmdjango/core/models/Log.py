6680e31a0d4b9927f708152aa0f37b2e18ec4398
cbrandt6 <cbrandt6@ycp.edu>
2021-03-17 18:39:03 -0400

    Removed runs field
    Due to the way Django handles many to one relationships, all we need is a foreign key in the Run table to accomplish the rel.

--- swarmdjango/core/models/Log.py
+++ swarmdjango/core/models/Log.py
@@ -5,6 +5,6 @@ from django.contrib.postgres.fields import JSONField
 class Log(models.Model):
     dateTime = models.DateTimeField()
     deviceID = models.TextField(default='NotSet')
-    runs = JSONField()
+    # Here would be the runs field, but Django just uses foreign keys
     filePath = models.TextField(default='NotSet')
     log = JSONField()
