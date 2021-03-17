2d0bd9b99bf04b676a1928c709aec829c91fe240
cbrandt6 <cbrandt6@ycp.edu>
2021-03-17 18:19:16 -0400

    Added new fields

--- swarmdjango/core/models/Run.py
+++ swarmdjango/core/models/Run.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from core.models import Log
 
 
 class Run(models.Model):
     dateTime = models.DateTimeField()
     deviceID = models.TextField(default='NotSet')
-    filePath = models.TextField(default='NotSet')
+    runID = models.IntegerField(default='NotSet')
+    logID = models.ForeignKey(Log)
     run = JSONField()
