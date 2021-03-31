0642da191470fc717190fb22e3d221a1463acd1d
Thomas McAdams <rithonn21@gmail.com>
2021-03-31 14:30:16 -0400

    Changes to the user model to reflect what is needed

--- swarmdjango/core/models/User.py
+++ swarmdjango/core/models/User.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.TextField(unique=True)
     password = models.TextField()
-    email = models.EmailField()
-    firstName = models.TextField()
-    lastName = models.TextField()
+    email = models.EmailField(unique=True)
+    firstName = models.CharField(max_length=20, blank=True)
+    lastName = models.TextField(max_length=20, blank=True)
     accountLevel = models.IntegerField(default=0);
+    isActive = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'password']
 
 # https://github.com/veryacademy/YT-Django-DRF-Simple-Blog-Series-JWT-Part-3/blob/master/django/users/models.py
